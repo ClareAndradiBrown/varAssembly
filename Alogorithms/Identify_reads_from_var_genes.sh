9081798378b814112063287a4ee9cd683ea29c82
ClareAndradiBrown <46680757+ClareAndradiBrown@users.noreply.github.com>
2023-03-12 09:03:21 +0000

    Update Identify_reads_from_var_genes.sh

--- Alogorithms/Identify_reads_from_var_genes.sh
+++ Alogorithms/Identify_reads_from_var_genes.sh
@@ -1,4 +1,4 @@
-
+#Assumes read files have their unique id separated by '_'
 
 #1. Identify reads derived from non-core genes
 #Map fastq files to human genome
@@ -11,7 +11,8 @@ subread-buildindex -o human_genome_index GRCh38.fasta
 subread-buildindex -o var_index var3kb_exon1.fasta
 
 
-#Map reads to human genome
+#Map reads to human genome and remove any reads that map to the human genome
+
 for file in *_1.fq
 do
 file_name=$(echo "$number_name" | cut -d"_" -f1)
@@ -22,7 +23,7 @@ seqtk subseq "$file_name"_1.fq "$file_name"_unmapped_reads.txt > "$file_name"_hu
 seqtk subseq "$file_name"_2.fq "$file_name"_unmapped_reads.txt > "$file_name"_human_unmapped_2.fq
 done
 
-#Map the human unmapped reads to Pf 3D7
+#Map the human unmapped reads to Pf 3D7 and identify any unmapped reads
 for file in *_human_unmapped_1.fq
 do
 file_name=$(echo "$number_name" | cut -d"_" -f1)
@@ -34,7 +35,7 @@ samtools sort -n plasmodium_3d7_mapped_novar/"$file_name"_mapped_pf_novar_mapped
 samtools view -f 4 plasmodium_3d7_mapped_novar/"$file_name"_mapped.bam  | cut -f1 > plasmodium_3d7_mapped_novar/"$file_name"_Pf_unmapped_reads.txt
 done
 
-#Map the human unmapped reads to var3kb exon 1
+#Map the human unmapped reads to var3kb exon 1 and identify any mapped reads
 for file in *_human_unmapped_1.fq
 do
 file_name=$(echo "$number_name" | cut -d"_" -f1)
@@ -49,7 +50,7 @@ sed 's/>//g' var3kb_exon1/"$file_name"_mapped_ids.txt > var3kb_exon1/"$file_name
 
 done
 
-#Check for duplication in reads identified and identify polymorphic reads
+#Check for duplication in reads identified (Pf unmapped + var3kb mapped) and identify polymorphic reads
 for file in *_human_unmapped_1.fq
 do
 file_name=$(echo "$number_name" | cut -d"_" -f1)
@@ -58,5 +59,8 @@ sort "$file_name"_polymorphic_reads_ids.txt| uniq -u > "$file_name"_polymorphic_
 seqtk subseq "$file_name"_1.fq "$file_name"_polymorphic_reads_ids_noDuplicates.txt > "$file_name"_polymorphic_reads_1.fq
 seqtk subseq "$file_name"_2.fq "$file_name"_polymorphic_reads_ids_noDuplicates.txt > "$file_name"_polymorphic_reads_2.fq
 done
+
+
+
 
 
