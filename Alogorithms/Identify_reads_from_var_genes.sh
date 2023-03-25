7e508cbee0488d0088cbd0f1064bf04604426adc
ClareAndradiBrown <46680757+ClareAndradiBrown@users.noreply.github.com>
2023-03-25 16:33:56 +0000

    Update Identify_reads_from_var_genes.sh

--- Alogorithms/Identify_reads_from_var_genes.sh
+++ Alogorithms/Identify_reads_from_var_genes.sh
@@ -19,8 +19,10 @@ file_name=$(echo "$file" | cut -d"_" -f1)
 subread-align -t 0 -i human_genome_index -r "$file_name"_1.fq -R "$file_name"_2.fq -o "$file_name"_human_mapped.sam --SAMoutput
 #extract unmapped reads
 samtools view -f 4 "$file_name"_human_mapped.sam | cut -f1 > "$file_name"_unmapped_reads.txt
-seqtk subseq "$file_name"_1.fq "$file_name"_unmapped_reads.txt > "$file_name"_human_unmapped_1.fq
-seqtk subseq "$file_name"_2.fq "$file_name"_unmapped_reads.txt > "$file_name"_human_unmapped_2.fq
+sort -u "$file_name"_unmapped_reads.txt > "$file_name"_unmapped_reads_noDup.txt
+
+seqtk subseq "$file_name"_1.fq "$file_name"_unmapped_reads_noDup.txt > "$file_name"_human_unmapped_1.fq
+seqtk subseq "$file_name"_2.fq "$file_name"_unmapped_reads_noDup.txt > "$file_name"_human_unmapped_2.fq
 done
 
 #Map the human unmapped reads to Pf 3D7 and identify any unmapped reads
@@ -49,7 +51,8 @@ for file in *_human_unmapped_1.fq
 do
 file_name=$(echo "$file" | cut -d"_" -f1)
 cat "$file_name"_read_mapped_ids.txt "$file_name"_Pf_unmapped_reads.txt > "$file_name"_polymorphic_reads_ids.txt
-sort "$file_name"_polymorphic_reads_ids.txt| uniq -u > "$file_name"_polymorphic_reads_ids_noDuplicates.txt
+sort -u "$file_name"_polymorphic_reads_ids.txt > "$file_name"_polymorphic_reads_ids_noDuplicates.txt
+
 seqtk subseq "$file_name"_1.fq "$file_name"_polymorphic_reads_ids_noDuplicates.txt > "$file_name"_polymorphic_reads_1.fq
 seqtk subseq "$file_name"_2.fq "$file_name"_polymorphic_reads_ids_noDuplicates.txt > "$file_name"_polymorphic_reads_2.fq
 done
